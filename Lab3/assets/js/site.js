a0770cf0ecee511345e37473300f124be913ec80
Aleks <akamenovic@hawk.iit.edu>
2022-10-18 13:22:21 -0500

    Add javascript functions

---
+++ Lab3/assets/js/site.js
@@ -0,0 +1,22 @@
+const form = document.getElementById('form');
+
+form.addEventListener('change', calculate);
+
+function calculate() {
+  const billTotal = document.getElementById('billTotal');
+  const tip = document.getElementById('tip');
+  const tipPercent = document.getElementById('tipPercent');
+  const tipAmount = document.getElementById('tipAmount');
+  const totalBillTip = document.getElementById('totalBillTip');
+
+  let x = 0;
+  let y = 0;
+
+  tipPercent.value = tip.value + "%";
+
+  x = parseFloat(billTotal.value) * parseFloat(tip.value/100);
+  tipAmount.value = x.toFixed(2);
+
+  y = parseFloat(billTotal.value) + parseFloat(tipAmount.value);
+  totalBillTip.value = y.toFixed(2);
+}
